0406770c6a900ad202236943be9e9c8ba20c7b86
yuralala8 <yc1435@nyu.edu>
2017-06-12 22:25:11 -0400

    oxford comma commit

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if
+    array.length < 3
+    results = array.join
+    results = array.join(" and ")
+    else
+    array.length >= 3
+    results = array[0..-2].join(", ") + ", and " + array[-1]
+    end
 end
+
